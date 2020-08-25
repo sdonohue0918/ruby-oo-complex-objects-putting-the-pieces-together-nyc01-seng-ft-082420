7764aa0ad6c483ec7ea1c684111ec9faf271c042
sdonohue0918 <sdonohue0918@gmail.com>
2020-08-25 17:31:12 -0400

    being wonky rn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
 
+require 'pry'
+
+
+class Book
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author
+    end
+    
+    
+
+    def author(author)
+        @author
+    end
+
+    def page_count=(num)
+        @num = num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    
+
+
+end
 
